90754963514e1723b7f4ae937f1d92f2080fcfe2
Mykyta Bobrov <mykyta.bobrov@avanade.com>
2019-03-22 15:28:48 +0100

    Implemented default and OnPush CD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChangeDetect';
+  public overall = 0;
+
+  public onChangeDetect() {
+    this.overall++;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentContainerComponent } from './containers/parent-container/parent-container.component';
+import { NestedComponentComponent } from './containers/nested-component/nested-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentContainerComponent,
+    NestedComponentComponent
   ],
   imports: [
     BrowserModule,
